f07af2355973b2accecad9e32a9a2bc24ee1ff23
valopezs <valeriabrilopez@gmail.com>
2022-10-31 20:27:30 -0500

    created user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const {getAllUsers, getUserById, createUser, updateUser, deleteUser, addFriend, removeFriend} = require("../../controllers/user-controller");
+
+// the api/user endpoint
+
+// get all users
+router.route("/").get(getAllUsers).post(createUser);
+
+// get users by id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// create friends by id
+router.route("/:userId/friends/friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
