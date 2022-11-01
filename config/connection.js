9e5b936c76f820861c8516001e2d44b838136e95
valopezs <valeriabrilopez@gmail.com>
2022-10-31 19:04:25 -0500

    setup mongodb connection, not done

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+    process.env.MONGODB_URI || "",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
