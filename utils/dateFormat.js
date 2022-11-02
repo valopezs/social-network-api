9a3d013d0fb418b22ac2b906015ee341b9e76e6e
valopezs <valeriabrilopez@gmail.com>
2022-11-01 22:31:09 -0500

    function to format timestamp

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -17,3 +17,51 @@ const addDateSuffix = (date) => {
     return dateStr;
 };
 
+// function to format a timestamp, accepts the timestamp and an `options` object as parameters
+module.exports = (
+    timestamp,
+    { monthLength = 'short', dateSuffix = true } = {}
+) => {
+    // month object
+    const months = {
+        0: monthLength === 'short' ? 'Jan' : 'January',
+        1: monthLength === 'short' ? 'Feb' : 'February',
+        2: monthLength === 'short' ? 'Mar' : 'March',
+        3: monthLength === 'short' ? 'Apr' : 'April',
+        4: monthLength === 'short' ? 'May' : 'May',
+        5: monthLength === 'short' ? 'Jun' : 'June',
+        6: monthLength === 'short' ? 'Jul' : 'July',
+        7: monthLength === 'short' ? 'Aug' : 'August',
+        8: monthLength === 'short' ? 'Sep' : 'September',
+        9: monthLength === 'short' ? 'Oct' : 'October',
+        10: monthLength === 'short' ? 'Nov' : 'November',
+        11: monthLength === 'short' ? 'Dec' : 'December',
+    };
+
+    const dateObj = new Date(timestamp);
+    const formattedMonth = months[dateObj.getMonth()];
+
+    const dayOfMonth = dateSuffix
+        ? addDateSuffix(dateObj.getDate())
+        : dateObj.getDate();
+
+    const year = dateObj.getFullYear();
+    let hour =
+        dateObj.getHours() > 12
+        ? Math.floor(dateObj.getHours() - 12)
+        : dateObj.getHours();
+
+    // when hour is 0 (12am), change it to 12
+    if (hour === 0) {
+        hour = 12;
+    }
+
+    const minutes = (dateObj.getMinutes() < 10 ? '0' : '') +dateObj.getMinutes;
+
+    // set AM or PM
+    const periodOfDay = dateObj.getHours() >= 12 ? 'PM' : 'AM';
+
+    const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
+
+    return formattedTimeStamp;
+}
